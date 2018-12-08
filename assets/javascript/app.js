8e66c79799f54545081c9a469d4de5b223be13d5
LACH <laurachg8@gmail.com>
2018-12-08 17:02:33 -0600

    Added my contact links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
     checkMenuScroll();
 
     $('section.parallax').parallax({
